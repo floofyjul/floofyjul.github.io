851e5a1196c349c5f0643c7b88cb534b1188c1b5
jul <julislazy@gmail.com>
2019-11-14 16:36:52 +0100

    republish stuffs, smooth scrolling

---
+++ resource/js/anims.js
@@ -0,0 +1,17 @@
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+function welcome() {
+  document.querySelector('#welcome').scrollIntoView({behavior: 'smooth'})
+}
+function about() {
+  document.querySelector('#about').scrollIntoView({behavior: 'smooth'})
+}
+function projects() {
+  document.querySelector('#projects').scrollIntoView({behavior: 'smooth'})
+}
+function contact() {
+  document.querySelector('#contact').scrollIntoView({behavior: 'smooth'})
+}
